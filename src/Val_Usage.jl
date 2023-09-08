3ab61bb163b587c80dbb0a72a7f97f9ebdc31bd8
WalterMadelim <143272683+WalterMadelim@users.noreply.github.com>
2023-09-08 09:46:27 +0800

    Create Val_Usage.jl

---
+++ src/Val_Usage.jl
@@ -0,0 +1,12 @@
+# dispatch based on values rather than Types
+julia> function foo(::Val{true}) "arg==true" end
+foo (generic function with 1 method)
+
+julia> function foo(::Val{false}) "arg==false" end
+foo (generic function with 2 methods)
+
+julia> g = foo ∘ Val
+foo ∘ Val
+
+julia> s1,s2 = g(true),g(false)
+("arg==true", "arg==false")
