38e70deb5c84f0a8466925314f2bf149c236ccdf
WalterMadelim <143272683+WalterMadelim@users.noreply.github.com>
2023-09-17 10:23:29 +0800

    Create JuMPusage.jl

---
+++ src/JuMPusage.jl
@@ -0,0 +1,14 @@
+# JuMP: basic usage
+using JuMP,Gurobi,AmplNLWriter
+model = Model()
+@variable(model,x≥0)
+@variable(model,0≤y≤3)
+@constraint(model,c1,6x+8y≥100)
+@constraint(model,c2,7x+12y≥120)
+@objective(model,Min,12x+20y+5)
+# print(model)
+set_optimizer(model, ()->AmplNLWriter.Optimizer("couenne"); add_bridges=true)
+# set_optimizer_attribute(model, MOI.Silent(), true)
+optimize!(model)
+
+#
