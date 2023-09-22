b6fec1311c17c87622f5899f9def7f3f4968a6ff
WalterMadelim <143272683+WalterMadelim@users.noreply.github.com>
2023-09-22 09:26:34 +0800

    Update change_a[1]_to.jl

--- src/change_a[1]_to.jl
+++ src/change_a[1]_to.jl
@@ -1,7 +1,49 @@
-# How to change a by using a function
 a = [11,22,33]
 function change_a1_to(w)
     a[1] = w
 end
 change_a2_to(w) = a[2] = w # EQ2: change_a2_again_to(w) = (a[2] = w)
 change_a3_to = w -> a[3] = w # EQ2: change_a2_Again_to = (w -> (a[3] = w))
+
+c = 4
+a = b = c # a,b are both 4::Int
+
+x() = y = z() = w = c # EQ2: x() = (y = (z() = (w = c)))
+
+julia> c = 4
+4
+
+julia> x() = y = z() = w = c
+x (generic function with 1 method)
+
+julia> y
+ERROR: UndefVarError: `y` not defined
+
+julia> z
+ERROR: UndefVarError: `z` not defined
+
+julia> w
+ERROR: UndefVarError: `w` not defined
+
+julia> x()
+(::var"#z#3") (generic function with 1 method)
+
+julia> z
+ERROR: UndefVarError: `z` not defined
+
+julia> w
+ERROR: UndefVarError: `w` not defined
+
+julia> y
+ERROR: UndefVarError: `y` not defined
+
+julia> y = 5
+5
+
+julia> x()
+(::var"#z#3") (generic function with 1 method)
+
+julia> y
+5
+
+
