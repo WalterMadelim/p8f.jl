e8ae6d7ed92722420434424b915d8195db749b5e
WalterMadelim <143272683+WalterMadelim@users.noreply.github.com>
2023-09-28 10:24:26 +0800

    Update downloadResourse.jl

--- src/downloadResourse.jl
+++ src/downloadResourse.jl
@@ -1,9 +1,9 @@
-j = 1270 # starting vol num
-d = 9 # try 10 files at one loop
+j = 1260
+d = 9
 for i in j:j+d
-    for y in 2020:2023 # year range
+    for y in 2020:2023
         local url = "https://pubsonline.informs.org/doi/pdf/10.1287/ijoc." * "$y" * "." * "$i" * "?download=true"
-        command = `cmd /c start $url`
+        local command = `cmd /c start $url`
         run(command)
     end
 end
