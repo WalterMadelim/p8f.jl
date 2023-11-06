7aea0fcb523dfb97332fa96dfbf28c34dcb055d5
WalterMadelim <143272683+WalterMadelim@users.noreply.github.com>
2023-11-06 21:34:51 +0800

    Update conv_QP_dual_pair.jl

--- src/conv_QP_dual_pair.jl
+++ src/conv_QP_dual_pair.jl
@@ -3,11 +3,18 @@ import MathOptInterface as MOI
 import Gurobi
 
 # conv_QP_dual_problems
+# primal_side matrix Q must be Positive Definite!
+# p437 nonlinear programming, Bertsekas
 # 06/11/23
 # to check if A is psd: we need to check all its eigenvalues
 # But we need to exclude Float_Point_Error!
 
-function pri_solve(decision_dim)
+function pri_solve(half_Q,c,A,b)
+    dim = size(A,2)
+    pri_solve(dim,half_Q,c,A,b)
+end
+
+function pri_solve(decision_dim,half_Q,c,A,b) # inner function
     @info "primal_conv_QP"
     o = Gurobi.Optimizer()
     x = MOI.add_variables(o,decision_dim)
@@ -26,7 +33,27 @@ function pri_solve(decision_dim)
     @info "" constraint_dual
 end
 
-function dual_solve(decision_dim)
+function dual_solve(half_Q::Matrix,c,A,b)
+    # check the psd (or pd) property
+    eigenvalues = LinearAlgebra.eigvals(half_Q);
+    @info "" eigenvalues
+    # dual_side_data
+    inv_of_halfQ = inv(half_Q)
+    @info "" inv_of_halfQ
+    half_P = .25 * A * inv_of_halfQ * A';
+    @info "" half_P
+    half_P = round.(half_P;digits = 10);
+    @info "" half_P
+    eigenvalues = LinearAlgebra.eigvals(half_P);
+    @warn "" eigenvalues
+    t = b .+ .5 * A * inv_of_halfQ * c
+    cnst_dual = .25 * c' * inv_of_halfQ * c
+    dim = length(t)
+    dual_solve(dim,half_P,t,cnst_dual)
+    return inv_of_halfQ, half_P, t, cnst_dual
+end
+
+function dual_solve(decision_dim::Int,half_P,t,cnst_dual) # inner function
     @info "dual_conv_QP"
     o = Gurobi.Optimizer()
     m = MOI.add_variables(o,decision_dim)
@@ -57,21 +84,5 @@ A = -[1. 2. 3.;
 0. 0. 1.]
 b = -[4.,1.,0.,0.,0.]
 
-# check the psd (or pd) property
-eigenvalues = LinearAlgebra.eigvals(half_Q);
-@info "" eigenvalues
-
-# dual_side_data
-tmp = inv(half_Q)
-half_P = .25 * A * tmp * A';
-@info "" half_P
-half_P = round.(half_P;digits = 10);
-@info "" half_P
-eigenvalues = LinearAlgebra.eigvals(half_P);
-@warn "" eigenvalues
-t = b .+ .5 * A * tmp * c
-cnst_dual = .25 * c' * tmp * c
-
-# go to check the strong duality and the dual_variables
-pri_solve(size(A,2))
-dual_solve(size(A,1))
+pri_solve(half_Q,c,A,b)
+inv_of_halfQ, half_P, t, cnst_dual = dual_solve(half_Q,c,A,b)
