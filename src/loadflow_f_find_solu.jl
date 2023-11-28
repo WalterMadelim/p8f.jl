859577feafbde7e639d19b0d1b04ff98ff848053
WalterMadelim <143272683+WalterMadelim@users.noreply.github.com>
2023-11-28 20:45:10 +0800

    Update loadflow_f_find_solu.jl

--- src/loadflow_f_find_solu.jl
+++ src/loadflow_f_find_solu.jl
@@ -1,20 +1,46 @@
 import NLsolve
+
+# calculate text-book-Y: 
+# off diag: -1/z
+# diagonal: collect half_b's related to this node, and * im; then add all (1/z)'s  [e.g., n = 4, related to node 1,3] 1/z43 + 1/z41 + im * hb43 + im * hb41
+
+# S = U * conj(I), Transmission complex power, where U is a node voltage phasor, I is through a z41.
+# S = abs2(U4) * conj(im * hb41), complex power goes to the underground through hb41
+# deltaS = abs2(I) * z41, complex power loss at z41
+
+
+
 # variables:
 # 1   2       3   4       5       6   7   8   9   10
 # P1, Q1,     Q2  V3      V4      V5  t2  t3  t4  t5
 
+
+
+
+
+P1 =   1.0010679820530304
+Q1 =   0.8095335677490827
+Q2 =  -0.5446790456016161
+V3 =   0.9783451836654884
+V4 =   0.9769502505106944
+V5 =   0.9692060417136963
+t2 =  -0.023467731252749847
+t3 =  -0.07023181102052245
+t4 =  -0.0755749506266199
+t5 =  -0.08877680685005919
+
 x0 = [1., .1,     .1,  1.,      1.,      1.,  0.,  0.,  0.,  0.]
 # knowns:
 V1 = 1.05
-t1 = 0.
+t1 = 0. # node 1 is V_theta node
 P2 = .5
-V2 = 1.0
+V2 = 1.0 # node 2 is PV node
 P3 = -.45
 Q3 = -.25
 P4 = -.4
 Q4 = -.05
 P5 = -.6
-Q5 = -.1
+Q5 = -.1 # node 3,4,5 are PV nodes
 
 function f(x)
 [
@@ -61,3 +87,39 @@ end
 
 sol = NLsolve.nlsolve(f,x0)
 sol.zero
+
+# u13 = V1 * exp(im * t1) - V3 * exp(im * t3)
+# u12 = V1 * exp(im * t1) - V2 * exp(im * t2)
+# z13 = LD["r"][2] + im * LD["x"][2]
+# z12 = LD["r"][1] + im * LD["x"][1]
+# s13 = abs2(u13) / conj(z13)
+# s12 = abs2(u12) / conj(z12)
+
+U1 = V1 * exp(im * t1)
+U2 = V2 * exp(im * t2)
+U3 = V3 * exp(im * t3)
+U4 = V4 * exp(im * t4)
+U5 = V5 * exp(im * t5)
+
+U = [U1,U2,U3,U4,U5]
+
+# The pi-equivalent branch model
+z = [LD["r"][b] + im * LD["x"][b] for b in 1:L]
+y = [im * LD["half_b"][b] for b in 1:L]
+Iz = [ ( U[ LD["from"][b] ] - U[ LD["to"][b] ] ) / z[b] for b in 1:L ]
+sz = [ U[ LD["from"][b] ] * conj( Iz[b] ) for b in 1:L ]
+Ill = [ U[ LD["from"][b] ] * y[b] for b in 1:L ]
+Ilr = [ U[ LD["to"][b] ] * y[b] for b in 1:L ]
+Iul = Iz .+ Ill
+Iur = Iz .- Ilr
+
+# I_shunt_13 = U1 * y_shunt_13
+# s1to3 = U1 * conj(I_z13 + I_shunt_13)
+
+# z12 = (LD["r"][1] + im * LD["x"][1])
+# I_z12 = (U1 - U2) / z12
+# y_shunt_12 = im * LD["half_b"][1]
+# I_shunt_12 = U1 * y_shunt_12
+# s1to2 = U1 * conj(I_z12 + I_shunt_12)
+
+- U[2] * conj(Iur[1])+ U[2] * conj(Iul[3])+ U[2] * conj(Iul[4])+ U[2] * conj(Iul[5])
