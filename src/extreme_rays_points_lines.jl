44284c4b63780aeb82f75d809acda5eee8ee51a6
WalterMadelim <143272683+WalterMadelim@users.noreply.github.com>
2024-08-27 08:44:55 +0800

    Update extreme_rays_points_lines.jl

--- src/extreme_rays_points_lines.jl
+++ src/extreme_rays_points_lines.jl
@@ -8,15 +8,10 @@ B = [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0; 1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.
 d = [10.401464507740426, 9.516722783088447, 10.205442753485626]
 M = 6
 I = 3
-function vecofvec2mat(vec)
-    Ncols = length(vec)
-    Nrows = length(vec[1])
-    mat = NaN * ones(Nrows, Ncols)
-    for c in eachindex(vec)
-        mat[:, c] .= vec[c]
-    end
-    mat
-end
+function ip(x, y) LinearAlgebra.dot(x, y) end
+function dirvec(x) x / LinearAlgebra.norm(x) end
+function vecofvec2mat(vec) return [vec[c][r] for r in eachindex(vec[1]), c in eachindex(vec)] end
+
 function ip(x, y) LinearAlgebra.dot(x, y) end
 m = JuMP.Model()
 JuMP.@variable(m, p[1:M] >= 0.)
@@ -27,3 +22,4 @@ vo = Polyhedra.vrep(po)
 @assert isempty(Polyhedra.lines(vo))
 ext_points = collect( Polyhedra.points(vo) )
 extMat = vecofvec2mat(ext_points)
+
