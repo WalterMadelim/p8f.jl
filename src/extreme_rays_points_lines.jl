dcc27b3cf367963dc7088a4126a522787fe60cf3
WalterMadelim <143272683+WalterMadelim@users.noreply.github.com>
2024-10-31 19:40:32 +0800

    Update extreme_rays_points_lines.jl

--- src/extreme_rays_points_lines.jl
+++ src/extreme_rays_points_lines.jl
@@ -23,3 +23,22 @@ vo = Polyhedra.vrep(po)
 ext_points = collect( Polyhedra.points(vo) )
 extMat = vecofvec2mat(ext_points)
 
+###########################################################
+
+# to remove redundant ( i.e. <= is < ) halfspace, do
+hsps = Polyhedra.hrep([Polyhedra.HalfSpace([-1, 0], 0), Polyhedra.HalfSpace([0, -1], 0), Polyhedra.HalfSpace([1/2, 1], 3), Polyhedra.HalfSpace([2, 1], 6), Polyhedra.HalfSpace([1, 1], 6.)])
+p = Polyhedra.polyhedron(hsps)
+v = Polyhedra.vrep(p) # middleman
+p1 = Polyhedra.polyhedron(v)
+h1 = Polyhedra.hrep(p1)
+hsps1 = h1.halfspaces # if no hyperplanes
+hsps1 = collect(Polyhedra.allhalfspaces(h1)) # hyperplanes => 2 halfspaces
+
+# to remove non-ext points, do
+pts = [[0, 0.], [0, 2.], [1, 0.], [1/3, 1]]
+v = Polyhedra.vrep(pts)
+p = Polyhedra.polyhedron(v)
+h = Polyhedra.hrep(p) # middleman
+p1 = Polyhedra.polyhedron(h)
+v1 = Polyhedra.vrep(p1)
+pts1 = v1.points.points
